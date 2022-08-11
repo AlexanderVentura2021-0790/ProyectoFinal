014257e19447718f839a200cb323b10e3da2b0ba
alexander ventura <venturaalexander0329@gmail.com>
2022-08-11 11:19:58 -0400

    primer commit

---
+++ src/ConfiguracionFirebase.js
@@ -0,0 +1,15 @@
+import firebase from "firebase/compat/app";
+import 'firebase/compat/firestore';
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBHFVqL5TwWuGEEPVNRwrGbYPXLwHy4y9s",
+  authDomain: "servidor-web-agenda.firebaseapp.com",
+  projectId: "servidor-web-agenda",
+  storageBucket: "servidor-web-agenda.appspot.com",
+  messagingSenderId: "495402242197",
+  appId: "1:495402242197:web:240da11c0f1cfe5b2b0b9c"
+};
+firebase.initializeApp(firebaseConfig);
+
+export const baseDeDato = firebase.firestore();
+export default firebase;
